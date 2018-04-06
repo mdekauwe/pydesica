5ff5b745bf396d0d2afee9c15c35a0f3c0df2f6c
mdekauwe <mdekauwe@gmail.com>
2018-04-06 19:46:26 +1000

    Added SW plot

--- src/run_gswp3_pixel.py
+++ src/run_gswp3_pixel.py
@@ -25,6 +25,37 @@ from desica import plot_time_to_mortality
 from desica import plot_transpiration
 from desica import plot_cwd
 
+def plot_sw(odir, out, time_step=30):
+
+    if time_step == 15:
+        ndays = out.t / 96
+    elif time_step == 30:
+        ndays = out.t / 48.
+    elif time_step == 60:
+        ndays = out.t / 24.
+
+    cb = ['#377eb8', '#ff7f00', '#4daf4a', \
+          '#f781bf', '#a65628', '#984ea3',\
+          '#999999', '#e41a1c', '#dede00']
+
+    fig = plt.figure(figsize=(9,6))
+    fig.subplots_adjust(hspace=0.3)
+    fig.subplots_adjust(wspace=0.2)
+    plt.rcParams['text.usetex'] = False
+    plt.rcParams['font.family'] = "sans-serif"
+    plt.rcParams['font.sans-serif'] = "Helvetica"
+    plt.rcParams['axes.labelsize'] = 12
+    plt.rcParams['font.size'] = 12
+    plt.rcParams['legend.fontsize'] = 10
+    plt.rcParams['xtick.labelsize'] = 12
+    plt.rcParams['ytick.labelsize'] = 12
+
+    ax1 = fig.add_subplot(111)
+    ax1.plot(ndays, out.sw, ls="-", color=cb[1])
+    ax1.set_ylabel("SWC (m$^{3}$ m$^{-3}$)")
+    ax1.set_xlabel("Time (days)")
+    fig.savefig("%s/sw.pdf" % (odir), bbox_inches='tight',
+                pad_inches=0.1)
 
 if __name__ == "__main__":
 
@@ -47,7 +78,7 @@ if __name__ == "__main__":
     met = met.reset_index(drop=True)
 
     psi_stem0 = 0. # initial stem water potential, MPa
-    AL = 6.        # plant leaf area, m2
+    AL = 3.        # plant leaf area, m2
     p50 = -4.      # xylem pressure inducing 50% loss of hydraulic conductivity
                    # due to embolism, MPa
     psi_f = -3.    # reference potential for Tuzet model, MPa
@@ -66,11 +97,13 @@ if __name__ == "__main__":
     deltaSv = 629.26
     Eaj = 29680.
     deltaSj = 631.88
+    soil_depth = 2.0
     F = Canopy(g1=g1, g0=g0, theta_J=theta_J, Rd25=Rd25, Q10=Q10,
                Vcmax25=Vcmax25, Jmax25=Jmax25, Eav=Eav, deltaSv=deltaSv,
                Eaj=Eaj, deltaSj=deltaSj)
     D = Desica(psi_stem0=psi_stem0, AL=AL, p50=p50, psi_f=psi_f, gmin=gmin,
-               Cl=Cl, Cs=Cs, F=F, g1=g1, nruns=2, stop_dead=True)
+               Cl=Cl, Cs=Cs, F=F, g1=g1, nruns=2, soil_depth=soil_depth,
+               stop_dead=True)
     out, day_of_death = D.run_simulation(met)
 
     odir = "plots"
@@ -80,3 +113,4 @@ if __name__ == "__main__":
     plot_time_to_mortality(odir, out, time_step)
     plot_transpiration(odir, out)
     plot_cwd(odir, out, time_step)
+    plot_sw(odir, out, time_step)
