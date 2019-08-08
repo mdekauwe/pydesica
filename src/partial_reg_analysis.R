b9dce069fc36e4e3db50c61356f1b191c4cad0ac
mdekauwe <mdekauwe@gmail.com>
2019-08-08 12:54:04 +1000

    Added script

---
+++ src/partial_reg_analysis.R
@@ -0,0 +1,23 @@
+#!/usr/bin/Rscript
+
+library(visreg)
+library(ggplot2)
+
+setwd("/Users/mdekauwe/src/python/pydesica/outputs")
+
+pft = "dsf"
+fname <- sprintf("%s_trait_sensitivity.csv", pft)
+df <- read.csv(fname)
+#head(df)
+
+fit <- lm(plc ~ gmin + AL + p50 + Cl + Cs + Tmax + Dmax, data=df)
+
+par(mfrow=c(2,4))
+visreg(fit, "Tmax")
+visreg(fit, "Dmax")
+visreg(fit, "gmin")
+visreg(fit, "AL")
+visreg(fit, "p50")
+visreg(fit, "Cl")
+visreg(fit, "Cs")
+
