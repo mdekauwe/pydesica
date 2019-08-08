43c959dcf409156e78c62bd5e15f9e2652643bda
mdekauwe <mdekauwe@gmail.com>
2019-08-08 10:33:40 +1000

    Added mp stuff

--- src/run_sensitivity_exp.py
+++ src/run_sensitivity_exp.py
@@ -28,84 +28,109 @@ from desica import plot_sw
 from desica import plot_transpiration_and_pet
 from get_params import get_params
 import itertools
+import multiprocessing as mp
 
-pft_name = "rf"
-params = get_params()
-pfts = list(params)
-p = params[pft_name]
+def main(ncpus=None):
 
-time_step = 30
-lat = -35.76
-lon = 148.0
-g0 = 0.0
-theta_J = 0.85
-Rd25 = 0.92
-Q10 = 1.92
-Vcmax25 = p.Vcmax
-Jmax25 = p.Jmax
-Eav = 58550.0
-deltaSv = 629.26
-Eaj = 29680.
-deltaSj = 631.88
-FAO = False
-psi_stem0 = -0.5
-psi_f = p.psiv
-kp_sat = p.kpsat
-g1 = p.g1
-s50 = p.s50
-sf = p.sf
+    params = get_params()
+    pfts = list(params)
 
-names = ['Tmax', 'Dmax', 'Dmean', 'gmin', 'AL', 'p50', 'Cl', 'Cs', \
-         'psi_stem', 'plc', 'day_of_death']
-df = pd.DataFrame(columns=names)
+    if ncpus is None: # use them all!
+        ncpus = mp.cpu_count()
 
-#Tmaxx = [30, 35, 40, 45]
-#RHx = [30, 20, 10, 5]
+    pool = mp.Pool(processes=ncpus)
 
-Tmaxx = [40]
-RHx = [10]
+    processes = []
+    for i, pft_name in enumerate(pfts):
 
-N = 10
-ranges = [
-    np.linspace(5, 15, N),        # gmin
-    np.linspace(1, 5, N),         # AL
-    np.linspace(-1, -6, N),       # p50
-    np.linspace(200, 800, N),     # Cl
-    np.linspace(10000, 50000, N), # Cs
-    np.linspace(0.5, 2.0, N)      # soil_depth
-]
+        px = params[pft_name]
+        p = mp.Process(target=worker, args=(pft_name, px, ))
+        processes.append(p)
 
-for Tmax in Tmaxx:
-    for RH in RHx:
-        met = generate_met_data(Tmin=15, Tmax=Tmax, RH=RH, ndays=500,
-                                lat=lat, lon=lon, time_step=time_step)
-        Dmax = np.max(met.vpd)
-        Dmean = np.mean(met.vpd)
+    # Run processes
+    for p in processes:
+        p.start()
 
-        for gmin, AL, p50, Cl, Cs, soil_depth, in itertools.product(*ranges):
+def worker(pft_name, p):
 
-            F = Canopy(g1=g1, g0=g0, theta_J=theta_J, Rd25=Rd25, Q10=Q10,
-                       Vcmax25=Vcmax25, Jmax25=Jmax25, Eav=Eav,
-                       deltaSv=deltaSv, Eaj=Eaj, deltaSj=deltaSj)
+    time_step = 30
+    lat = -35.76
+    lon = 148.0
+    g0 = 0.0
+    theta_J = 0.85
+    Rd25 = 0.92
+    Q10 = 1.92
+    Vcmax25 = p.Vcmax
+    Jmax25 = p.Jmax
+    Eav = 58550.0
+    deltaSv = 629.26
+    Eaj = 29680.
+    deltaSj = 631.88
+    FAO = False
+    psi_stem0 = -0.5
+    psi_f = p.psiv
+    kp_sat = p.kpsat
+    g1 = p.g1
+    s50 = p.s50
+    sf = p.sf
 
-            D = Desica(psi_stem0=psi_stem0, AL=AL, p50=p50, psi_f=psi_f,
-                       gmin=gmin, Cl=Cl, Cs=Cs, F=F, g1=g1, stop_dead=True,
-                       FAO=FAO, kp_sat=kp_sat, s50=s50, sf=sf,
-                       soil_depth=soil_depth)
+    names = ['Tmax', 'Dmax', 'Dmean', 'gmin', 'AL', 'p50', 'Cl', 'Cs', \
+             'psi_stem', 'plc', 'day_of_death']
+    df = pd.DataFrame(columns=names)
 
-            out, day_of_death = D.run_simulation(met)
-            psi_stem = out.psi_stem.iloc[-1]
-            plc = out.plc.iloc[-1]
+    #Tmaxx = [30, 35, 40, 45]
+    #RHx = [30, 20, 10, 5]
 
-            result = [Tmax, Dmax, Dmean, gmin, AL, p50, Cl, Cs,  \
-                      psi_stem, plc, day_of_death]
+    Tmaxx = [40]
+    RHx = [10]
 
-            s = pd.Series(result, index=df.columns)
-            df = df.append(s, ignore_index=True)
+    N = 2
+    ranges = [
+        np.linspace(5, 15, N),        # gmin
+        np.linspace(1, 5, N),         # AL
+        np.linspace(-1, -6, N),       # p50
+        np.linspace(200, 800, N),     # Cl
+        np.linspace(10000, 50000, N), # Cs
+        np.linspace(0.5, 2.0, N)      # soil_depth
+    ]
 
-odir = "outputs"
-if not os.path.exists(odir):
-    os.makedirs(odir)
+    for Tmax in Tmaxx:
+        for RH in RHx:
+            met = generate_met_data(Tmin=15, Tmax=Tmax, RH=RH, ndays=500,
+                                    lat=lat, lon=lon, time_step=time_step)
+            Dmax = np.max(met.vpd)
+            Dmean = np.mean(met.vpd)
 
-ofname = os.path.join(odir, "%s_trait_sensitivity.csv" % (pft_name))
-df.to_csv(ofname, index=False)
+            for gmin, AL, p50, Cl, Cs, soil_depth, in \
+                itertools.product(*ranges):
+
+                F = Canopy(g1=g1, g0=g0, theta_J=theta_J, Rd25=Rd25, Q10=Q10,
+                           Vcmax25=Vcmax25, Jmax25=Jmax25, Eav=Eav,
+                           deltaSv=deltaSv, Eaj=Eaj, deltaSj=deltaSj)
+
+                D = Desica(psi_stem0=psi_stem0, AL=AL, p50=p50, psi_f=psi_f,
+                           gmin=gmin, Cl=Cl, Cs=Cs, F=F, g1=g1, stop_dead=True,
+                           FAO=FAO, kp_sat=kp_sat, s50=s50, sf=sf,
+                           soil_depth=soil_depth)
+
+                out, day_of_death = D.run_simulation(met)
+                psi_stem = out.psi_stem.iloc[-1]
+                plc = out.plc.iloc[-1]
+
+                result = [Tmax, Dmax, Dmean, gmin, AL, p50, Cl, Cs,  \
+                          psi_stem, plc, day_of_death]
+
+                s = pd.Series(result, index=df.columns)
+                df = df.append(s, ignore_index=True)
+
+    odir = "outputs"
+    if not os.path.exists(odir):
+        os.makedirs(odir)
+
+    ofname = os.path.join(odir, "%s_trait_sensitivity.csv" % (pft_name))
+    df.to_csv(ofname, index=False)
+
+
+if __name__ == "__main__":
+
+    main()
