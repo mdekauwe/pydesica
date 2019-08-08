f9a0f17945bec61b86c6d4c732db8016b399c2c7
mdekauwe <mdekauwe@gmail.com>
2019-08-08 13:59:04 +1000

    Added relative importance

--- src/partial_reg_analysis.R
+++ src/partial_reg_analysis.R
@@ -2,6 +2,8 @@
 
 library(visreg)
 library(ggplot2)
+library(ppcor)
+library(relaimpo) 
 
 setwd("/Users/mdekauwe/src/python/pydesica/outputs")
 
@@ -10,16 +12,28 @@ fname <- sprintf("%s_trait_sensitivity.csv", pft)
 df <- read.csv(fname)
 #head(df)
 
-fit <- lm(plc ~ gmin + AL + p50 + Cl + Cs + Tmax + Dmax, data=df)
-visreg(fit)
+#fit <- lm(plc ~ gmin + AL + p50 + Cl + Cs + Tmax + Dmax, data=df)
+fit <- lm(plc ~ gmin + AL + p50 + Cl + Cs, data=df)
+
+#visreg(fit)
+par(mfrow=c(2,4))
+visreg(fit, "Tmax")
+visreg(fit, "Dmax")
+visreg(fit, "gmin")
+visreg(fit, "AL")
+visreg(fit, "p50")
+visreg(fit, "Cl")
+visreg(fit, "Cs")
+
 summary(fit)
 
-#par(mfrow=c(2,4))
-#visreg(fit, "Tmax")
-#visreg(fit, "Dmax")
-#visreg(fit, "gmin")
-#visreg(fit, "AL")
-#visreg(fit, "p50")
-#visreg(fit, "Cl")
-#visreg(fit, "Cs")
+ 
+rel_imp <- calc.relimp(fit, type=c("lmg","last", "first"), rela=TRUE)
+plot(rel_imp)
+
+boot <- boot.relimp(fit, b=1000, type=c("lmg","last", "first"),
+                    rank=TRUE, diff=TRUE, rela=TRUE)
+#booteval.relimp(boot) 
+plot(booteval.relimp(boot, sort=TRUE))
+
 
