14c4eda18ef618f672a6f90d79726fed8a5c13cd
mdekauwe <mdekauwe@gmail.com>
2019-08-16 16:55:14 +1000

    Added refilling

--- src/desica.py
+++ src/desica.py
@@ -123,11 +123,13 @@ class Desica(object):
         doy = 0
         for i in range(1, n):
 
-            # Impose pre-dawn (6 am) xylem refilling 
+            # Impose pre-dawn (6 am) xylem refilling
             if hod == 12 and out.psi_stem[i-1] > -4:
                 # assuming psi_stem can refill 50% of distance to psi_soil
                 refill = np.abs(out.psi_stem[i-1] - out.psi_soil[i-1]) * 0.5
                 out.psi_stem[i-1] += refill
+
+                # Ensure we can't refill above psi_soil
                 out.psi_stem[i-1] = min(out.psi_stem[i-1], out.psi_soil[i-1])
 
                 # assume complete refilling
