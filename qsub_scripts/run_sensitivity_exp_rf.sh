bc4cfe22efdba3e8ae7c5ff85de1c08e3169cd24
mdekauwe <mdekauwe@gmail.com>
2019-09-04 13:32:14 +1000

    Added qsub script

---
+++ qsub_scripts/run_sensitivity_exp_rf.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#PBS -m ae
+#PBS -P w35
+#PBS -q normalbw
+#PBS -M mdekauwe@gmail.com
+#PBS -l mem=16GB
+#PBS -l ncpus=16
+#PBS -l walltime=00:05:00
+#PBS -l wd
+#PBS -j oe
+#PBS -l other=gdata1
+
+module load dot
+source activate sci
+
+python src/run_sensitivity_exp.py "rf"
